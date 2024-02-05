7aae983768712478eeb4bde85c9c8572c9b5ea65
Your Name <you@example.com>
2024-02-05 20:17:21 +0530

    first commit

---
+++ src/main/java/lk/ijse/Controller/WelcomeController.java
@@ -0,0 +1,44 @@
+package lk.ijse.Controller;
+
+import lk.ijse.Server.Server;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class WelcomeController {
+    @FXML
+    private AnchorPane root;
+
+    public void btnSigninOnAction(ActionEvent actionEvent) {
+        try {
+            Server server = Server.getServerSocket();
+            Thread thread = new Thread(server);
+            thread.start();
+
+            root.getChildren().clear();
+            Stage stage = (Stage) root.getScene().getWindow();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/login.fxml"))));
+            stage.show();
+            stage.setOnCloseRequest(e-> {
+                System.exit(0);
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void btnSignupOnAction(ActionEvent actionEvent) throws IOException {
+        root.getChildren().clear();
+        Stage stage = (Stage) root.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/register.fxml"))));
+        stage.setTitle("Sign Up Form");
+        stage.show();
+
+    }
+}
---
+++ src/main/java/lk/ijse/Launcher.java
@@ -0,0 +1,19 @@
+package lk.ijse;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/welcome_page.fxml"))));
+        stage.setTitle("welcome page");
+        stage.centerOnScreen();
+        stage.show();
+    }
+}
